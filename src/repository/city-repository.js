1f2291451caa7898d6378fa8c110133d204ba6bb
imshukla10 <ritesh.1923ec1099@kiet.edu>
2024-03-07 12:33:12 +0530

    City repo modified

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -9,6 +9,28 @@ class CityRepository{
         }
     }
 
+    async updateCity(cityId,data){
+        try{
+            const city = await City.update(data,{
+                where:{
+                    id:cityId
+                }
+            })
+        } catch(error){
+            throw {error}
+        }
+    }
+
+
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityId)
+            return city
+        } catch(error){
+            throw {error}
+        }
+    }
+
     async deleteCity(cityId){
         try {
             await City.destroy({
